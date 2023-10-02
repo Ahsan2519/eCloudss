8b1963b7d5df6108b43e77ab2ddd714bac196670
ahsanpsychx <ansariahsan67@gmail.com>
2023-10-03 01:28:27 +0530

    Fix: Counter logic fixes

--- src/Reducers/CounterReducer.jsx
+++ src/Reducers/CounterReducer.jsx
@@ -1,35 +1,33 @@
 import {
-    INCREMENT_CUSTOMER_COUNT,
-    INCREMENT_EXPERIENCE_COUNT,
-    INCREMENT_PROJECTS_COUNT,
-  } from '../Actions/CounterAction';
-  
-  const initialState = {
-    customerCount: 0,
-    experienceCount: 0,
-    projectsCount: 0,
-  };
-  
-  const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case INCREMENT_CUSTOMER_COUNT:
-        return {
-          ...state,
-          customerCount: Math.min(state.customerCount + 800, 136664),
-        };
-      case INCREMENT_EXPERIENCE_COUNT:
-        return {
-          ...state,
-          experienceCount: Math.min(state.experienceCount + 800, 3),
-        };
-      case INCREMENT_PROJECTS_COUNT:
-        return {
-          ...state,
-          projectsCount: Math.min(state.projectsCount + 800, 136664),
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default counterReducer;
+  INCREMENT_CUSTOMER_COUNT,
+  INCREMENT_EXPERIENCE_COUNT,
+  INCREMENT_PROJECTS_COUNT,
+} from '../Actions/CounterAction';
+
+const initialState = {
+  customerCount: 0,
+  experienceCount: 0,
+  projectsCount: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT_CUSTOMER_COUNT:
+    case INCREMENT_EXPERIENCE_COUNT:
+    case INCREMENT_PROJECTS_COUNT:
+      const customerIncrement = Math.min(state.customerCount + 136664 / 500, 136664);
+      const experienceIncrement = Math.min(state.experienceCount + 3 / 500, 3);
+      const projectsIncrement = Math.min(state.projectsCount + 136664 / 500, 136664);
+
+      return {
+        ...state,
+        customerCount: parseFloat(customerIncrement.toFixed(2)),
+        experienceCount: parseFloat(experienceIncrement.toFixed(2)),
+        projectsCount: parseFloat(projectsIncrement.toFixed(2)),
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
